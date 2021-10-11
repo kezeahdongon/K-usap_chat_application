513ff12de6333f4aeebd0510839622427318ce38
kezeahdongon <kezeahdongon54@gmail.com>
2021-10-11 22:44:37 +0800

    push

--- index.js
+++ index.js
@@ -1,46 +1,5 @@
-const APP = (() => {
-    document.addEventListener('DOMContentLoaded', init);
+const loginBtn = document.querySelector('#Loginbtn');
 
-    function init(){
-        document.querySelector('#Loginbtn').addEventListener() => {
-            location.replace(url + './pages/login/index.html')
-        }
-        document.querySelector('#Signupbtn').addEventListener() => {
-            location.replace(url + './pages/signup/index.html')
-        }
-    }
-
-    // INTERACTIONS
-    function addEventListeners() {
-        const elbtnA = document.querySelector('#btnA');
-        const blogElement = document.querySelector('#all_blogs');
-        // SHOW MORE
-        elbtnA.addEventListener('click', () => {
-            const blog = firebase_blog(currentShown);
-            blogElement.innerHTML += blogItem(blog.id,blog.title,blog.content);
-            currentShown += 1;
-        });
-    }
-
-    function getData(blogCount,cb) {
-        const blogs = firebase_blogs(blogCount);
-        cb(blogs)
-    }
-
-    // INITIAL VIEW
-    function afterFetch(blogs) {
-        let blogElement = document.querySelector('#all_blogs');
-        
-        let blogElementContent = '';
-
-        for(let i = 0; i < blogs.length; i++) {
-            blogElementContent += blogItem(blogs[i].id,blogs[i].title,blogs[i].content);
-        }
-        blogElement.innerHTML = blogElementContent;
-    }
-
-    return {
-        getData,
-        afterFetch
-    }
-})();
+loginBtn.addEventListener('click', e => {
+    location.replace('./pages/login/index.html')
+});
